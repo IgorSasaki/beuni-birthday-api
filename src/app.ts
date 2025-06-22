5d40458b0ff40080ececf351e3430b1940025067
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 02:43:35 -0300

    feat(app): atach database in server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
+import 'reflect-metadata'
 import express from 'express'
+
+import { AppDataSource } from './config/data-source'
 
 const app = express()
 
 app.use(express.json())
+
+AppDataSource.initialize()
+  .then(() => console.log('Database connected 📦'))
+  .catch(err => console.log(err))
 
 export default app
