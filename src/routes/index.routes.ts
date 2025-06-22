608a90839a15e4289e4bd2a2f42594adce9a1429
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 13:46:12 -0300

    feat(routes): atach employee routes and functions

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express'
 
 import { AuthController } from '@/controllers/auth.controller'
+import { EmployeeController } from '@/controllers/employee.controller'
+import { authenticate } from '@/middlewares/auth.middleware'
 
 const AppRoutes = Router()
 
 const authController = new AuthController()
+const employeeController = new EmployeeController()
 
 AppRoutes.post('/users', authController.register)
 AppRoutes.post('/auth', authController.login)
+
+AppRoutes.post('/employees', authenticate, employeeController.createEmployee)
+AppRoutes.get('/employees', authenticate, employeeController.getEmployees)
 
 export default AppRoutes
