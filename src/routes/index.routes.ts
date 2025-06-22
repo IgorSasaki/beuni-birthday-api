b51bc24cd6117e4f775b1757a879339d809253b7
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 14:49:27 -0300

    feat(routes): attach routes and functions

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,17 +2,28 @@ import { Router } from 'express'
 
 import { AuthController } from '@/controllers/auth.controller'
 import { EmployeeController } from '@/controllers/employee.controller'
+import { GiftsController } from '@/controllers/gifts.controller'
 import { authenticate } from '@/middlewares/auth.middleware'
 
 const AppRoutes = Router()
 
 const authController = new AuthController()
 const employeeController = new EmployeeController()
+const giftsController = new GiftsController()
 
 AppRoutes.post('/users', authController.register)
 AppRoutes.post('/auth', authController.login)
 
 AppRoutes.post('/employees', authenticate, employeeController.createEmployee)
 AppRoutes.get('/employees', authenticate, employeeController.getEmployees)
+AppRoutes.get(
+  '/employees/:employeeId',
+  authenticate,
+  employeeController.getEmployeeById
+)
+
+AppRoutes.post('/gifts', authenticate, giftsController.createGiftRequest)
+AppRoutes.get('/gifts', authenticate, giftsController.getGifts)
+AppRoutes.put('/gifts/:giftId', authenticate, giftsController.updateGiftStatus)
 
 export default AppRoutes
