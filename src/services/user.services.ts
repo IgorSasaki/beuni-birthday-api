abac62345221bc9ee8fb9cb4ba54bef2e4b52e57
IgorSasaki <igor-sasaki@hotmail.com>
2025-06-22 03:25:23 -0300

    feat(services user.service): implement user service

---
+++ src/services/user.services.ts
@@ -0,0 +1,19 @@
+import { Repository } from 'typeorm'
+
+import { User } from '@/entities/User'
+
+export class UserService {
+  private repository: Repository<User>
+
+  constructor(repository: Repository<User>) {
+    this.repository = repository
+  }
+
+  async createUser(user: User) {
+    return await this.repository.save(user)
+  }
+
+  async findUserByEmail(email: string) {
+    return await this.repository.findOneBy({ email })
+  }
+}
